5c5f68db78245e4149c30be2235a769063d50e4b
rjosephp <rajesh.joseph@gmail.com>
2021-07-20 14:36:16 -0700

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -10,8 +10,9 @@ exports.calculate = function(req, res) {
     res.json({ error: err.message });
   });
 
+  //Using +operator prevents tring concat
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
